8863f70ba2e2a0d38325662dc683509f740cf75e
Lebon Lucas <lebon233@unicaen.fr>
2026-01-20 08:39:21 +0100

    exercice 2

--- TP1/generalisation/template_method.py
+++ TP1/generalisation/template_method.py
@@ -8,21 +8,33 @@ class Site():
     self.units = p_units
     self.rate = p_rate
 
+  def getBaseAmount(self):
+    raise NotImplementedError
+  
+  def getTaxeAmount(self):
+    raise NotImplementedError
+  
+  def getBillableAmount(self):
+    return self.getBaseAmount() + self.getTaxeAmount()
+
 
 
 class ResidentialSale(Site):
 
-  def getBillableAmount(self):
-    base = self.units * self.rate
-    tax = base * self.tax_rate
-    return base + tax
+  def getBaseAmount(self):
+    return self.units * self.rate
+  
+  def getTaxeAmount(self):
+    return self.getBaseAmount() * self.tax_rate
 
 
 class LifelineSite(Site):
-  def getBillableAmount(self):
-    base = self.units * self.rate * 0.5
-    tax = base * self.tax_rate * 0.2
-    return base + tax
+
+  def getBaseAmount(self):
+    return self.units * self.rate * 0.5
+  
+  def getTaxeAmount(self):
+    return self.getBaseAmount() * self.tax_rate * 0.2
 
 
 resident = ResidentialSale(5000, 1.5)
