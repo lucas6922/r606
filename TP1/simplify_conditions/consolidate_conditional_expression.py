afa8d561272548879c7a32468da0ac3770607c58
Lebon Lucas <lebon233@unicaen.fr>
2026-01-20 08:55:21 +0100

    exo 2

--- TP1/simplify_conditions/consolidate_conditional_expression.py
+++ TP1/simplify_conditions/consolidate_conditional_expression.py
@@ -1,4 +1,8 @@
 def disabilityAmount():
+  if (seniority < 2 or monthsDisabled > 12 or isPartTime):
+    return 0
+  
+  '''
   if seniority < 2:
     return 0
   if monthsDisabled > 12:
@@ -7,3 +11,4 @@ def disabilityAmount():
     return 0
     # Compute the disability amount.
     # ...
+  '''
--- TP1/simplify_conditions/decompose_conditional.py
+++ TP1/simplify_conditions/decompose_conditional.py
@@ -1,4 +1,7 @@
-if date.before(SUMMER_START) or date.after(SUMMER_END):
+if not dateBetween(SUMMER_START,SUMMER_END):
     charge = quantity * winterRate + winterServiceCharge
 else:
     charge = quantity * summerRate
+
+def dateBetween(start_date, end_date):
+    return date.after(start_date) or date.before(end_date)
